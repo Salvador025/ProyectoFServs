a3eaf0da3fb09c17e41659ea5dd5fdeef464b190
joseAcevesG <119601853+joseAcevesG@users.noreply.github.com>
2024-03-15 23:05:15 +0000

    danger test

--- dangerfile.js
+++ dangerfile.js
@@ -5,49 +5,49 @@ message("Changed Files in this PR: \n - " + modifiedMD);
 
 // Function to check the commit message format according to Conventional Commits
 function checkCommitMessageFormat(commit) {
-	const lines = commit.message.split("\n");
-	const [title, emptyLine, ...description] = lines;
+  const lines = commit.message.split("\n");
+  const [title, emptyLine, ...description] = lines;
 
-	// Regex to validate Conventional Commits format
-	const ccFormatRegex =
-		/^(feat|fix|docs|styles|refactor|test|chore|perf|build|ci|revert)(\(\w+\))?:\s/;
-	const maxLength = 80;
+  // Regex to validate Conventional Commits format
+  const ccFormatRegex =
+    /^(feat|fix|docs|styles|refactor|test|chore|perf|build|ci|revert)(\(\w+\))?:\s/;
+  const maxLength = 80;
 
-	// Check 1: Validate Conventional Commit format
-	if (!ccFormatRegex.test(title)) {
-		fail(
-			`Commit title does not follow Conventional Commits format: "${title}"`
-		);
-	}
+  // Check 1: Validate Conventional Commit format
+  if (!ccFormatRegex.test(title)) {
+    fail(
+      `Commit title does not follow Conventional Commits format: "${title}"`,
+    );
+  }
 
-	// Check 2: Validate title length
-	if (title.length > maxLength) {
-		fail(`Commit title exceeds ${maxLength} characters: "${title}"`);
-	}
+  // Check 2: Validate title length
+  if (title.length > maxLength) {
+    fail(`Commit title exceeds ${maxLength} characters: "${title}"`);
+  }
 
-	// Check 2: Empty line between title and description (if description exists)
-	if (description.length > 0 && emptyLine !== "") {
-		fail("Commit description does not have an empty line after title.");
-	}
+  // Check 2: Empty line between title and description (if description exists)
+  if (description.length > 0 && emptyLine !== "") {
+    fail("Commit description does not have an empty line after title.");
+  }
 
-	// Check 3: Description has at least 5 characters
-	// if (description.length > 1 && description[1].trim().length < 5) {
-	// 	fail("Commit description is less than 5 characters.");
-	// }
+  // Check 3: Description has at least 5 characters
+  //   if (description.length > 1 && description[1].trim().length < 5) {
+  //     warn("Commit description is less than 5 characters.");
+  //   }
 
-	// Check 4: Each line in the description does not have more than 80 characters
-	description.forEach((line, index) => {
-		if (line.length > 80) {
-			fail(
-				`Line ${
-					index + 1
-				} in commit description exceeds 72 characters: "${line.substring(
-					0,
-					69
-				)}..."`
-			);
-		}
-	});
+  // Check 4: Each line in the description does not have more than 80 characters
+  description.forEach((line, index) => {
+    if (line.length > 80) {
+      fail(
+        `Line ${
+          index + 1
+        } in commit description exceeds 72 characters: "${line.substring(
+          0,
+          69,
+        )}..."`,
+      );
+    }
+  });
 }
 
 // Iterate over each commit to check the message format
