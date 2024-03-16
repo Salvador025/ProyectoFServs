efef5b2831fb6152caf2114786dd19dc89d97f21
joseAcevesG <119601853+joseAcevesG@users.noreply.github.com>
2024-03-16 02:56:42 +0000

    feat(users): :sparkles: users model
    creation of the users model

---
+++ src/models/user.ts
@@ -0,0 +1,19 @@
+import { Schema, model } from "mongoose";
+
+const schema = new Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+});
+
+export default model("User", schema);
