3ebf5d75cf82f092d8c940f7fd9e4892cc0c6ca9
joseAcevesG <119601853+joseAcevesG@users.noreply.github.com>
2024-04-05 21:20:56 +0000

    feat(types): :label: update DecodeToken and Create User, RequestUser
    new types created and update of DecodedToken

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,10 +1,22 @@
+import { Request } from "express";
 export type InputToken = {
 	name: string;
 	email: string;
 };
 
-interface DecodedToken {
+export interface DecodedToken {
 	email: string;
 	iat: number;
 	name: string;
+}
+
+export interface User {
+	name: string;
+	password: string;
+	email: string;
+	role: string;
+}
+
+export interface RequestUser extends Request {
+	user?: User;
 }
