6a8c081304bafb3516524afb9a2221039a208e9c
joseAcevesG <jose.aceves@iteso.mx>
2024-04-06 12:25:26 -0600

    feat(users): send user to check it works

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,14 +1,15 @@
-import { Router, Request, Response } from "express";
+import { Router, Response } from "express";
 import usersController from "../controllers/users.controller";
 import auth from "../middlewares/auth";
 
+import { RequestUser } from "../types";
 const router = Router();
 
 router.post("/signup", usersController.signUp);
 router.post("/login", usersController.logIn);
 
-router.get("/:username", auth, (req: Request, res: Response) =>
-	res.send("User details"),
+router.get("/:username", auth, (req: RequestUser, res: Response) =>
+	res.send(req.user),
 );
 
 export default router;
