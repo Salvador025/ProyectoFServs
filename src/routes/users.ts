359232660a97463edbf605c691a8845241b0fbb0
joseAcevesG <jose.aceves@iteso.mx>
2024-04-06 13:58:47 -0600

    feat(users): :sparkles: update role route created
    new rote to change user role

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,15 +1,14 @@
 import { Router, Response } from "express";
 import usersController from "../controllers/users.controller";
 import auth from "../middlewares/auth";
-
 import { RequestUser } from "../types";
+
 const router = Router();
 
-router.post("/signup", usersController.signUp);
-router.post("/login", usersController.logIn);
-
-router.get("/:username", auth, (req: RequestUser, res: Response) =>
+router.get("/:username", (req: RequestUser, res: Response) =>
 	res.send(req.user),
 );
+
+router.put("/updateRole", auth, usersController.changeRole);
 
 export default router;
