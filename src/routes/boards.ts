b3cbb870a0f894b81a6fbe0c417b33d586be7cfe
joseAcevesG <jose.aceves@iteso.mx>
2024-04-06 14:08:02 -0600

    feat(board): :sparkles: CRUD of boards routes
    routes of the borad using roles moddleware

--- src/routes/boards.ts
+++ src/routes/boards.ts
@@ -1,21 +1,39 @@
 import { Router, Request, Response } from "express";
+import roles from "../middlewares/roles";
+import Roles from "../types/roles";
 
 const router = Router();
 
-router.get("/", (req: Request, res: Response) => {
-	res.send("Index routes works!!!!");
+router.get("/:username", (req: Request, res: Response) => {
+	res.send(`User ${req.params.username} boards`);
 });
 
-router.post("/upload", (req: Request, res: Response) => {
-	res.send("Endpoint para subir archivos!!!!");
+router.post(
+	"/:username",
+	roles(Roles.Creator, Roles.Admin),
+	(req: Request, res: Response) => {
+		res.send("Board created");
+	},
+);
+
+router.get("/:username/:id", (req: Request, res: Response) => {
+	res.send(`Board ${req.params.id}`);
 });
 
-router.put("/upload", (req: Request, res: Response) => {
-	res.send("Endpoint para subir archivos!!!!");
-});
+router.put(
+	"/:username/:id",
+	roles(Roles.Creator, Roles.Admin),
+	(req: Request, res: Response) => {
+		res.send(`Board ${req.params.id} updated`);
+	},
+);
 
-router.delete("/upload", (req: Request, res: Response) => {
-	res.send("Endpoint para subir archivos!!!!");
-});
+router.delete(
+	"/:username/:id",
+	roles(Roles.Creator, Roles.Admin),
+	(req: Request, res: Response) => {
+		res.send(`Board ${req.params.id} deleted`);
+	},
+);
 
 export default router;
