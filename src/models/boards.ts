36366d839b7bab03997f1019d5c663b891d0e69a
joseAcevesG <119601853+joseAcevesG@users.noreply.github.com>
2024-04-19 14:10:46 -0600

    feat(boards): :sparkles: boards schema created (#22)
    schema for the boards created

--- src/models/boards.ts
+++ src/models/boards.ts
@@ -1,5 +1,19 @@
 import { Schema, model } from "mongoose";
 
-const boardsSchema = new Schema({});
+const boardsSchema = new Schema({
+	name: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	owner: {
+		type: Schema.Types.ObjectId,
+		required: true,
+	},
+	direction: {
+		type: String,
+		required: true,
+	},
+});
 
 export default model("users", boardsSchema);
