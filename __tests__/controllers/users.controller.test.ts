283023c1a72727a5b23ac1dff53ef68541677d54
joseAcevesG <119601853+joseAcevesG@users.noreply.github.com>
2024-04-19 23:28:04 -0600

    Test User controller (#25)
    * fix(users): :adhesive_bandage: fix response on error
    
    send the correct status when the server fail
    
    * feat(board): :label: new type board
    
    type board crated
    
    * feat(board): :sparkles: controlrs for create a board
    
    board upload to s3 and save on DB
    
    * feat(test): test for crate boards on api test
    
    new request for create a board
    
    * feat(test): :white_check_mark: test for login
    
    teest for invalid credentialls and error
    
    * test(test): :white_check_mark: test for changeRole
    
    test for valid change, invalid role and error
    
    * test(test): :white_check_mark: test for uploadProfilePicture
    
    test valid case and error case
    
    * refactor(test): :recycle: change to user.findOne to keep consistency
    
    change to keep consistency of the rest of the test

--- __tests__/controllers/users.controller.test.ts
+++ __tests__/controllers/users.controller.test.ts
@@ -4,6 +4,9 @@ import UsersController from "../../src/controllers/users.controller";
 import user from "../../src/models/user";
 import ResponseStatus from "../../src/types/response-codes";
 import { code as createToken } from "../../src/utils/create-token";
+import { RequestUser, User } from "../../src/types";
+import Roles from "../../src/types/roles";
+import { Readable } from "stream";
 
 // Mock the external dependencies
 jest.mock("../../src/models/user", () => ({
@@ -15,6 +18,7 @@ jest.mock("../../src/models/user", () => ({
 			email: "test@example.com",
 		}),
 	),
+	updateOne: jest.fn().mockResolvedValue(Promise.resolve("")),
 }));
 
 jest.mock("../../src/utils/hash-password", () =>
@@ -26,26 +30,26 @@ jest.mock("../../src/utils/create-token", () => ({
 }));
 
 describe("UsersController", () => {
-	let req: Partial<Request>;
-	let res: Partial<Response>;
-
-	// Mock the Express req and res objects before each test
-	beforeEach(() => {
-		req = {
-			body: {
-				name: "TestUser",
-				password: "password123", // pragma: allowlist secret
-				email: "test@example.com",
-			},
-		};
-
-		res = {
-			status: jest.fn().mockReturnThis(),
-			send: jest.fn().mockReturnThis(),
-		};
-	});
-
 	describe("signUp", () => {
+		let req: Partial<Request>;
+		let res: Partial<Response>;
+
+		// Mock the Express req and res objects before each test
+		beforeEach(() => {
+			req = {
+				body: {
+					name: "TestUser",
+					password: "password123", // pragma: allowlist secret
+					email: "test@example.com",
+				},
+			};
+
+			res = {
+				status: jest.fn().mockReturnThis(),
+				send: jest.fn().mockReturnThis(),
+			};
+		});
+
 		test("should create a new user and send a CREATED response", async () => {
 			// Arrange
 			// Cast the req object to the correct type
@@ -115,6 +119,24 @@ describe("UsersController", () => {
 	});
 
 	describe("logIn", () => {
+		let req: Partial<Request>;
+		let res: Partial<Response>;
+
+		// Mock the Express req and res objects before each test
+		beforeEach(() => {
+			req = {
+				body: {
+					name: "TestUser",
+					password: "password123", // pragma: allowlist secret
+					email: "test@example.com",
+				},
+			};
+
+			res = {
+				status: jest.fn().mockReturnThis(),
+				send: jest.fn().mockReturnThis(),
+			};
+		});
 		test("should log in an existing user and send back a token", async () => {
 			// Arrange
 			(user.findOne as jest.Mock).mockResolvedValue({
@@ -133,6 +155,175 @@ describe("UsersController", () => {
 			expect(createToken).toHaveBeenCalled();
 			expect(res.status).toHaveBeenCalledWith(ResponseStatus.SUCCESS);
 			expect(res.send).toHaveBeenCalledWith({ token: "token" });
+		});
+
+		test("should handle invalid credentials", async () => {
+			// Arrange
+			(user.findOne as jest.Mock).mockResolvedValue(null);
+
+			// Act
+			await UsersController.logIn(req as Request, res as Response);
+
+			// Assert
+			expect(res.status).toHaveBeenCalledWith(ResponseStatus.UNAUTHORIZED);
+			expect(res.send).toHaveBeenCalledWith("Invalid credentials");
+		});
+
+		test("should handle generic errors", async () => {
+			// Arrange
+			(user.findOne as jest.Mock).mockRejectedValue(new Error("Generic error"));
+
+			// Act
+			try {
+				await UsersController.logIn(req as Request, res as Response);
+			} catch (error) {
+				// Assert
+				expect(res.status).toHaveBeenCalledWith(
+					ResponseStatus.INTERNAL_SERVER_ERROR,
+				);
+				expect(res.send).toHaveBeenCalledWith("Something went wrong");
+			}
+		});
+	});
+
+	describe("changeRole", () => {
+		let req: Partial<RequestUser>;
+		let res: Partial<Response>;
+
+		// Mock the Express req and res objects before each test
+		beforeEach(() => {
+			req = {
+				user: {
+					name: "TestUser",
+					username: "testuser",
+					email: "test@example.com",
+					role: Roles.USER,
+				},
+				body: {
+					role: "admin",
+				},
+			};
+			res = {
+				status: jest.fn().mockReturnThis(),
+				send: jest.fn().mockReturnThis(),
+			};
+			(user.updateOne as jest.Mock).mockResolvedValue(
+				Promise.resolve("Role changed"),
+			);
+		});
+		test("should change the role of a user", async () => {
+			(user.updateOne as jest.Mock).mockResolvedValue({
+				name: "TestUser",
+				username: "testuser",
+				email: "test@example.com",
+			});
+
+			await UsersController.changeRole(req as RequestUser, res as Response);
+
+			// Assert
+			expect(res.status).toHaveBeenCalledWith(ResponseStatus.SUCCESS);
+			expect(res.send).toHaveBeenCalledWith("Role changed");
+		});
+
+		test("should handle invalid role", async () => {
+			// Arrange
+			req.body.role = "invalid_role";
+
+			// Act
+			await UsersController.changeRole(req as RequestUser, res as Response);
+
+			// Assert
+			expect(res.status).toHaveBeenCalledWith(ResponseStatus.BAD_REQUEST);
+			expect(res.send).toHaveBeenCalledWith("Invalid role");
+		});
+
+		test("should handle generic errors", async () => {
+			// Arrange
+			(user.updateOne as jest.Mock).mockRejectedValue(
+				new Error("Generic error"),
+			);
+			try {
+				// Act
+				await UsersController.changeRole(req as RequestUser, res as Response);
+			} catch (error) {
+				// Assert
+				expect(res.status).toHaveBeenCalledWith(ResponseStatus.BAD_REQUEST);
+				expect(res.send).toHaveBeenCalledWith("Something went wrong");
+			}
+		});
+	});
+
+	describe("uploadProfilePicture", () => {
+		let req: Partial<RequestUser & { file: { location: string } }>;
+		let res: Partial<Response>;
+
+		// Mock the Express req and res objects before each test
+		beforeEach(() => {
+			req = {
+				user: {
+					name: "TestUser",
+					username: "testuser",
+					email: "test@example.com",
+					role: Roles.USER,
+				},
+				file: {
+					fieldname: "",
+					originalname: "",
+					encoding: "",
+					mimetype: "",
+					size: 0,
+					destination: "",
+					filename: "",
+					path: "",
+					buffer: Buffer.from([]),
+					location: "profile_picture.jpg",
+					stream: new Readable(),
+				},
+			};
+			res = {
+				status: jest.fn().mockReturnThis(),
+				send: jest.fn().mockReturnThis(),
+			};
+			(user.updateOne as jest.Mock).mockResolvedValue(
+				Promise.resolve("image Upload"),
+			);
+		});
+
+		test("should upload profile picture and send a SUCCESS response", async () => {
+			// Arrange
+			(user.updateOne as jest.Mock).mockResolvedValue({});
+
+			await UsersController.uploadProfilePicture(
+				req as RequestUser & { file: { location: string } },
+				res as Response,
+			);
+			// Assert
+			expect((req.user as User).image).toBe("profile_picture.jpg");
+			expect(user.updateOne).toHaveBeenCalledWith(
+				{ email: "test@example.com" },
+				{ image: "profile_picture.jpg" },
+			);
+			expect(res.status).toHaveBeenCalledWith(ResponseStatus.SUCCESS);
+			expect(res.send).toHaveBeenCalledWith("Profile picture uploaded");
+		});
+
+		test("should handle errors and send a BAD_REQUEST response", async () => {
+			// Arrange
+			(user.updateOne as jest.Mock).mockRejectedValue(
+				new Error("Generic error"),
+			);
+
+			try {
+				// Act
+				await UsersController.uploadProfilePicture(
+					req as RequestUser & { file: { location: string } },
+					res as Response,
+				);
+			} catch (error) {
+				// Assert
+				expect(res.status).toHaveBeenCalledWith(ResponseStatus.BAD_REQUEST);
+				expect(res.send).toHaveBeenCalledWith("Something went wrong");
+			}
 		});
 	});
 });
