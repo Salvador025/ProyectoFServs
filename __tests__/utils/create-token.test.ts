2d1b6eb12e32fda414f77a0d50dea9c6d6b68d15
joseAcevesG <119601853+joseAcevesG@users.noreply.github.com>
2024-04-26 21:46:51 +0000

    fix(test): :recycle: add describe for create-token
    surround test with file name

--- __tests__/utils/create-token.test.ts
+++ __tests__/utils/create-token.test.ts
@@ -5,30 +5,32 @@ import { InputToken } from "../../src/types";
 import setUpLogs from "../../src/utils/logs";
 setUpLogs();
 
-describe("code", () => {
-	it("should return a valid token", () => {
-		const data: InputToken = { name: "test", email: "test@test.com" };
-		const token = code(data);
-		expect(token).toBeDefined();
-	});
-});
-
-describe("decode", () => {
-	it("should return decoded token if valid", () => {
-		const token =
-			// cspell: disable-next-line
-			"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoidGVzdCIsImVtYWlsIjoidGVzdEB0ZXN0LmNvbSIsImlhdCI6MTcxMjE4ODgwNH0.E-Yiml2gLBA6oK9BuFF850-2I7AxpBdgD8iJAMWMcZA"; // pragma: allowlist secret
-		const decodedToken = decode(token);
-		expect(decodedToken).toEqual({
-			name: "test",
-			email: "test@test.com",
-			iat: 1712188804,
+describe("create-token", () => {
+	describe("code", () => {
+		it("should return a valid token", () => {
+			const data: InputToken = { name: "test", email: "test@test.com" };
+			const token = code(data);
+			expect(token).toBeDefined();
 		});
 	});
 
-	it("should return null if token is invalid", () => {
-		const token = "invalid_token";
-		const decodedToken = decode(token);
-		expect(decodedToken).toBeNull();
+	describe("decode", () => {
+		it("should return decoded token if valid", () => {
+			const token =
+				// cspell: disable-next-line
+				"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoidGVzdCIsImVtYWlsIjoidGVzdEB0ZXN0LmNvbSIsImlhdCI6MTcxMjE4ODgwNH0.E-Yiml2gLBA6oK9BuFF850-2I7AxpBdgD8iJAMWMcZA"; // pragma: allowlist secret
+			const decodedToken = decode(token);
+			expect(decodedToken).toEqual({
+				name: "test",
+				email: "test@test.com",
+				iat: 1712188804,
+			});
+		});
+
+		it("should return null if token is invalid", () => {
+			const token = "invalid_token";
+			const decodedToken = decode(token);
+			expect(decodedToken).toBeNull();
+		});
 	});
 });
