ee9511baecd6cbfebaa8e4715702fa1ac9c5d0de
joseAcevesG <jose.aceves@iteso.mx>
2024-05-01 16:07:42 -0600

    feat(views): :recycle: module masic-load
    module to check for user data and token

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,118 +1,122 @@
-document.getElementById("formSignUp").addEventListener("submit", (event) => {
-	event.preventDefault();
+import basicLoad from "/assets/scripts/basic-load.js";
 
-	const username = document.querySelector("#singUp #username_singUp");
-	const name = document.querySelector("#singUp #name_singUp");
-	const email = document.querySelector("#singUp #email_singUp");
-	const password = document.querySelector("#singUp #password_singUp");
-
-	const formData = {
-		username: username.value,
-		name: name.value,
-		email: email.value,
-		password: password.value,
-	};
-
-	fetch("auth/signup", {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify(formData),
-	})
-		.then((response) => {
-			if (!response.ok) {
-				return response.text().then((text) => {
-					throw new Error(text);
-				});
-			}
-			$("#singUp").modal("hide");
-			name.value = "";
-			email.value = "";
-			password.value = "";
-			return response.text();
-		})
-		.then((data) => {
-			return fetch("auth/login", {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify(formData),
-			});
-		})
-		.then((response) => {
-			if (!response.ok) {
-				return response.text().then((text) => {
-					throw new Error(text);
-				});
-			}
-			return response.json();
-		})
-		.then((data) => {
-			sessionStorage.setItem("token", data.token);
-
-			console.log(data.token);
-		})
-		.catch((error) => {
-			console.error(error.message);
-			alert(error.message);
-		});
-});
-
-document.getElementById("formLogIn").addEventListener("submit", (event) => {
-	event.preventDefault();
-
-	const email = document.querySelector("#logIn #email_logIn");
-	const password = document.querySelector("#logIn #password_logIn");
-
-	const formData = {
-		email: email.value,
-		password: password.value,
-	};
-
-	fetch("auth/login", {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify(formData),
-	})
-		.then((response) => {
-			if (!response.ok) {
-				return response.text().then((text) => {
-					throw new Error(text);
-				});
-			}
-			$("#logIn").modal("hide");
-			email.value = "";
-			password.value = "";
-			return response.json();
-		})
-		.then((data) => {
-			sessionStorage.setItem("token", data.token);
-
-			console.log(data.token);
-		})
-		.catch((error) => {
-			console.error(error.message);
-			alert(error.message);
-		});
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-	debugger;
+document.addEventListener("DOMContentLoaded", () => {
 	const token = sessionStorage.getItem("token");
+	debugger;
 	if (window.location.pathname === "/") {
 		if (token) {
 			window.location.href = "/home";
 			return;
 		}
+		document
+			.getElementById("formSignUp")
+			.addEventListener("submit", (event) => {
+				event.preventDefault();
+
+				const username = document.querySelector("#singUp #username_singUp");
+				const name = document.querySelector("#singUp #name_singUp");
+				const email = document.querySelector("#singUp #email_singUp");
+				const password = document.querySelector("#singUp #password_singUp");
+
+				const formData = {
+					username: username.value,
+					name: name.value,
+					email: email.value,
+					password: password.value,
+				};
+
+				fetch("auth/signup", {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify(formData),
+				})
+					.then((response) => {
+						if (!response.ok) {
+							return response.text().then((text) => {
+								throw new Error(text);
+							});
+						}
+						$("#singUp").modal("hide");
+						name.value = "";
+						email.value = "";
+						password.value = "";
+						return response.text();
+					})
+					.then((data) => {
+						return fetch("auth/login", {
+							method: "POST",
+							headers: {
+								"Content-Type": "application/json",
+							},
+							body: JSON.stringify(formData),
+						});
+					})
+					.then((response) => {
+						if (!response.ok) {
+							return response.text().then((text) => {
+								throw new Error(text);
+							});
+						}
+						return response.json();
+					})
+					.then((data) => {
+						sessionStorage.setItem("token", data.token);
+
+						console.log(data.token);
+						window.location.href = "/home";
+					})
+					.catch((error) => {
+						console.error(error.message);
+						alert(error.message);
+					});
+			});
+
+		document.getElementById("formLogIn").addEventListener("submit", (event) => {
+			event.preventDefault();
+
+			const email = document.querySelector("#logIn #email_logIn");
+			const password = document.querySelector("#logIn #password_logIn");
+
+			const formData = {
+				email: email.value,
+				password: password.value,
+			};
+
+			fetch("auth/login", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(formData),
+			})
+				.then((response) => {
+					if (!response.ok) {
+						return response.text().then((text) => {
+							throw new Error(text);
+						});
+					}
+					$("#logIn").modal("hide");
+					email.value = "";
+					password.value = "";
+					return response.json();
+				})
+				.then((data) => {
+					sessionStorage.setItem("token", data.token);
+					console.log(data.token);
+					window.location.href = "/home";
+				})
+				.catch((error) => {
+					console.error(error.message);
+					alert(error.message);
+				});
+		});
 		return;
 	}
 
-	if (!token) {
-		window.location.href = "/";
-		return;
-	}
+	basicLoad().then((data) => {
+		debugger;
+		console.log(data);
+	});
 });
