5beb084ec1aa8d9e6c95dbdc802603d0122b447d
Salvador025 <jose.mariscal@iteso.mx>
2024-05-08 01:11:09 -0600

    fix: :zap: bug with a import not used

--- src/routes/google.ts
+++ src/routes/google.ts
@@ -3,11 +3,40 @@ import passport from "passport";
 import googleController from "../controllers/google.controller";
 const router = Router();
 
+/**
+ * @swagger
+ * tags:
+ *  name: Google
+ *  description: Google authentication
+ */
+
 router.get(
 	"/",
 	passport.authenticate("google", { scope: ["profile", "email"] }),
 );
 
+/**
+ * @swagger
+ * /auth/google/callback:
+ * get:
+ * summary: Google callback
+ * tags: [Google]
+ * description: Google callback
+ * 	security:
+ * 		- bearerAuth: []
+ * responses:
+ * 	"200":
+ * 		description: Google callback
+ * 		content:
+ * 			application/json:
+ * 				schema:
+ * 					type: object
+ * 					properties:
+ * 						message:
+ * 							type: string
+ * 							example: Google callback
+ * 	"401":
+ */
 router.get(
 	"/callback",
 	passport.authenticate("google", { failureRedirect: "/auth/login" }),
--- src/routes/views.ts
+++ src/routes/views.ts
@@ -2,11 +2,26 @@ import { Router } from "express";
 
 const router = Router();
 
+/**
+ * @swagger
+ * tags:
+ *  name: Views
+ *  description: Views endpoints
+ */
+
 router.get("/home", (req, res) => {
 	res.render("home", {
 		layout: "main",
 	});
 });
+
+/*swagger documentation*/
+/**
+ * @swagger
+ * tags:
+ *  name: Views
+ *  description: Views endpoints
+ */
 
 router.get("/settings", (req, res) => {
 	res.render("settings", {
@@ -28,17 +43,54 @@ router.get("/marketplace", (req, res) => {
 	});
 });
 
+/**
+ * @swagger
+ * /frontCallback/{token}:
+ *  get:
+ *   summary: Front callback
+ *   tags: [Views]
+ *   description: Front callback
+ *   parameters:
+ *    - in: path
+ *      name: token
+ *      required: true
+ *      description: The token
+ *      schema:
+ *       type: string
+ *   responses:
+ *    200:
+ *     description: Front callback
+ */
+
 router.get("/frontCallback/:token", (req, res) => {
 	res.render("empty", {
 		layout: "frontCallback",
 	});
 });
 
+/**
+ * @swagger
+ * /play:
+ *  get:
+ *   summary: Play
+ *   tags: [Views]
+ *   description: Play endpoint
+ */
+
 router.get("/play", (req, res) => {
 	res.render("play", {
 		layout: "main",
 	});
 });
+
+/**
+ * @swagger
+ * /createBoard:
+ *  get:
+ *   summary: Create board
+ *   tags: [Views]
+ *   description: Create board endpoint
+ */
 
 router.get("/createBoard", (req, res) => {
 	res.render("create-data", {
