4bfd13503aad24b2822b65ad75b6cbf22defd3d6
joseAcevesG <jose.aceves@iteso.mx>
2024-05-08 01:20:38 -0600

    feat: :construction: work in progress

--- __tests__/features/search.feature
+++ __tests__/features/search.feature
@@ -1,9 +1,13 @@
-Feature: ITESO Search
-  As a user
-  I want to search for "Carreras" on the ITESO homepage
-  So that I can see the search results page
+Feature: Log into the Web Application
 
-  Scenario: Searching for "Carreras" on ITESO
-    Given I am on the ITESO homepage
-    When I search for "Carreras"
-    Then I should see the search results
+  As a user of the web application
+  I want to log in using my credentials
+  So that I can access the home page
+
+  Scenario: Successful login
+    Given I am on the page "http://localhost:3000"
+    When I click on the "logInButton" button
+    And I enter the email "test@test.com" in the email field
+    And I enter the password "123" in the password field
+    And I press Enter
+    Then I should be redirected to "http://localhost:3000//home"
---
+++ __tests__/steps/search.steps.ts
@@ -0,0 +1,47 @@
+import { defineFeature, loadFeature } from "jest-cucumber";
+import webdriver from "selenium-webdriver";
+import path from "path";
+
+const feature = loadFeature(
+	path.resolve(__dirname, "../features/search.feature"),
+);
+
+defineFeature(feature, (test) => {
+	let driver: webdriver.ThenableWebDriver;
+	beforeAll(async () => {
+		driver = new webdriver.Builder().forBrowser("chrome").build();
+	});
+	test("Successful login", ({ given, when, and, then }) => {
+		given(/^I am on the page "(.*)"$/, async (arg0) => {
+			await driver.get(arg0);
+		});
+
+		when(/^I click on the "(.*)" button$/, async (arg0) => {
+			await driver.findElement(webdriver.By.id(arg0)).click();
+		});
+
+		and(/^I enter the email "(.*)" in the email field$/, async (arg0) => {
+			await driver.findElement(webdriver.By.id("email_logIn")).sendKeys(arg0);
+		});
+
+		and(/^I enter the password "(.*)" in the password field$/, async (arg0) => {
+			await driver
+				.findElement(webdriver.By.id("password_logIn"))
+				.sendKeys(arg0);
+		});
+
+		and("I press Enter", async () => {
+			await driver
+				.findElement(webdriver.By.id("password_logIn"))
+				.sendKeys(webdriver.Key.ENTER);
+		});
+
+		then(/^I should be redirected to "(.*)"$/, async (arg0) => {
+			await driver.wait(webdriver.until.urlContains(arg0), 1000);
+		});
+	});
+
+	afterAll(async () => {
+		await driver.quit();
+	});
+});
